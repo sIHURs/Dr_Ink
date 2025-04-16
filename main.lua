7c097b7b027bf75bd3decc1e04fd85e5c8500cbc
yifandeUbuntu <yifan.wang2@stud.uni-hannover.de>
2025-04-16 23:24:57 +0200

    add gamemenu demo

---
+++ main.lua
@@ -0,0 +1,20 @@
+
+function love.load()
+
+    love.window.setMode( 960, 720)
+    require("menu")
+    table.insert(menu.buttons, newButton("Start Game", function() print("Starting Gameing") end))
+    table.insert(menu.buttons, newButton("Load Game", function() print("Load Game") end))
+    table.insert(menu.buttons, newButton("Settings", function() print("Setting") end))
+    table.insert(menu.buttons, newButton("Exit", function() love.event.quit(0) end))
+
+    ShowMenu()
+end 
+
+function love.update(dt)
+    UpdateMenu()
+end 
+
+function love.draw()
+    DrawMenu()
+end 
